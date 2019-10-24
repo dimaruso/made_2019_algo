5369e9af997be94cf457fb721f69e75f2b396d0e
dimaruso <54507670+dimaruso@users.noreply.github.com>
2019-10-24 22:33:25 +0300

    Add deque

---
+++ 01/z_1_2.cpp
@@ -0,0 +1,168 @@
+#include <iostream>
+#include <cstring>
+//#include <ctime>
+class MyNotList
+{
+
+public:
+	MyNotList();
+	~MyNotList();
+	void push_back(int data);
+	void push_front(int data);
+	int pop_back();
+	int pop_front();
+	void prin();
+private:	
+	int* dataset;
+	int size;
+	int len;
+	int head;
+	int tail;
+protected:
+	void upData();
+};
+	void MyNotList::prin() {
+		
+		for (int i = 0; i < len; i++)
+			std::cout << dataset[i] << " ";
+		std::cout << std::endl;
+	};
+	MyNotList::MyNotList() :
+		head(0),
+		tail(0),
+		dataset(new int[8]),
+		size(0),
+		len(8)
+	{
+	}
+	MyNotList::~MyNotList()
+	{
+		delete[] dataset;
+	}
+	void MyNotList::upData()
+	{
+		int newLen = len * 2;
+		int *newDataset = new int[newLen];
+		if (head < tail) {
+			memcpy(newDataset, dataset, size*sizeof(int));
+		}
+		else {
+			memcpy(newDataset+size-head, dataset, (tail + 1)*sizeof(int));
+			memcpy(newDataset, dataset+head, (size - head) * sizeof(int));
+			head = 0;
+			tail = size - 1;
+		}
+		delete[] dataset;
+		dataset = newDataset;
+		len = newLen;
+	}
+	int MyNotList::pop_front()
+	{
+		if (size == 0) return -1;
+		else if (size == 1)
+		{
+			size--;
+			return dataset[tail];
+		}
+		else
+		{
+			int value = dataset[head];
+			if (head == len - 1) head = 0;
+			else head++;
+			size--;
+			return value;
+		}
+	}
+	void MyNotList::push_back(int data)
+	{
+		if (size == len) upData();
+
+		if (size == 0) {
+			dataset[tail] = data;
+			size++;
+		}
+		else
+		{
+			if (tail == len -1)	tail = 0;
+			else tail++;
+
+			size++;
+			dataset[tail] = data;
+		}
+		
+	}
+	void MyNotList::push_front(int data)
+	{
+		if (size == len) upData();
+
+		if (size == 0) {
+			
+			dataset[head] = data;
+			size++;
+		}
+		else
+		{
+			if (head == 0) head = len - 1;
+			else head--;
+			dataset[head] = data;
+			size++;
+		}
+	}
+	int MyNotList::pop_back()
+	{
+		if (size == 0) return -1;
+		else if (size == 1)
+		{
+			size--;
+			return dataset[tail];
+		}
+		else 
+		{
+			int value = dataset[tail];
+
+			if (tail == 0)	tail = len - 1;
+			else tail--;
+			size--;
+			return value;
+		}
+	}
+
+int main() {
+
+	MyNotList q;
+	int n;
+	int com;
+	int value;
+	int res = 1;
+	std::cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		std::cin >> com >> value;
+		if (res)
+		switch (com)
+		{
+		case 1:
+			q.push_front(value);
+			break;
+		case 2:
+			if (q.pop_front() != value) res = 0;
+			break;
+		case 3:
+			q.push_back(value);
+			break;
+		case 4:
+			if (q.pop_back() != value) res = 0;
+			break;
+		default:
+			//res = 0;
+			break;
+		}
+	}
+	if (res) 
+		std::cout << "YES" << std::endl;
+	else 
+		std::cout << "NO" << std::endl;
+	//q.~MyNotList();
+	//system("pause");
+	return 0;
+}
