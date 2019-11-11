9e1774a1c6a0aa5c3507441a56e06bc18c930d1c
dimaruso <54507670+dimaruso@users.noreply.github.com>
2019-11-11 12:06:17 +0300

    Update

--- 03/z_6_2.cpp
+++ 03/z_6_2.cpp
@@ -1,19 +1,23 @@
 #include <iostream>
 #include <stack>
+
 class BinTree
 {
 public:
 	BinTree();
 	~BinTree();
-	struct Node;
 	void Insert(const int value);
-	Node* Find(int const value) const;
-	Node* FindMin(int const value) const;
-	void DFS() const;
+	void DeleteNode(const int value);
+	void DFS(void(*TakeValue)(const int value))const;
+	bool Find(int const value)const;
+	int FindMin()const;
 private:
 	size_t Count;
+	struct Node;
 	Node* Root;
-	void DFSN(Node* node) const;
+	void DFSN(Node* node, void(*TakeValue)(const int value))const;
+	Node* FindN(int const value)const;
+	Node* FindMinN()const;
 };
 
 struct BinTree::Node
@@ -95,7 +99,79 @@ void BinTree::Insert(const int value)
 	}
 }
 
-BinTree::Node* BinTree::Find(int const value) const
+void BinTree::DeleteNode(const int value)
+{
+
+		Node* last = Root; //Parent
+		Node* cur = Root;
+
+		if (Root->Key == value)
+		{
+			if (!Root->Left)
+			{
+				Root = Root->Right;
+			}
+			else if (!Root->Right)
+			{
+				Root = Root->Left;
+			}
+		}
+		//Find Parent & current
+		while (last && value != last->Key && (last->Left || last->Right))
+		{
+			if (value < last->Key)
+			{
+				if (last->Left && last->Left->Key == value)
+				{
+					cur = last->Left;
+					break;
+				}
+				last = last->Left;
+			}
+			else if (value > last->Key)
+			{
+				if (last->Right && last->Right->Key == value)
+				{
+					cur = last->Right;
+					break;
+				}
+				last = last->Right;
+			}
+		}
+
+		//delete current
+		if (cur->Key == value)
+		{
+			while (cur)
+			{
+				if (!cur->Left)
+				{
+					if (last->Left == cur)
+						last->Left = cur->Right;
+					else if (last->Right == cur)
+						last->Right = cur->Right;
+					break;
+				}
+				else if (!cur->Right)
+				{
+					if (last->Left == cur)
+						last->Left = cur->Left;
+					else if (last->Right == cur)
+						last->Right = cur->Left;
+					break;
+				}
+				else
+				{
+					last = cur;
+					cur->Key = cur->Right->Key;
+					cur = cur->Right;
+				}
+			}
+			delete cur;
+		}
+}
+
+BinTree::Node* BinTree::FindN(int const value)const
 {
 	Node* last = Root;
 	while (value != last->Key && (last->Left || last->Right))
@@ -105,10 +181,20 @@ BinTree::Node* BinTree::Find(int const value) const
 		else if (value > last->Key)
 			last = last->Right;
 	}
-	return last;
+	if (last->Key == value)
+		return last;
+	else
+		return nullptr;
 }
 
-BinTree::Node* BinTree::FindMin(int const value) const
+bool BinTree::Find(int const value)const
+{
+	Node* last = FindN(value);
+	return (last && last->Key == value);
+}
+
+
+BinTree::Node* BinTree::FindMinN()const
 {
 	Node* cur = Root;
 	if (cur)
@@ -117,22 +203,32 @@ BinTree::Node* BinTree::FindMin(int const value) const
 	return cur;
 }
 
-void BinTree::DFS() const
+int BinTree::FindMin()const
 {
-	DFSN(Root);
+	Node* cur = FindMinN();
+	if (cur)
+		return cur->Key;
+	else
+	{
+		throw std::out_of_range("Tree is empty");
+		return -1;
+	}
+}
+
+void BinTree::DFS( void(*TakeValue)(const int value))const
+{
+	DFSN(Root, TakeValue);
 	return;
 }
 
-void BinTree::DFSN(Node* node) const
+void BinTree::DFSN(Node* node, void(*TakeValue)(const int value))const
 {
 	
 	std::stack<Node*> s;
-	Node* cur = Root;
+	Node* cur = node;
 
 	while (true)
 	{
-		if (cur->Key == 10)
-			s.empty();
 		while (cur)
 		{
 			s.push(cur);
@@ -145,7 +241,7 @@ void BinTree::DFSN(Node* node) const
 			cur = s.top();
 			if (cur->Right)
 			{
-				std::cout << s.top()->Key << " ";
+				TakeValue(s.top()->Key);
 				s.pop();
 				cur = cur->Right;
 			}
@@ -153,13 +249,13 @@ void BinTree::DFSN(Node* node) const
 			{
 				while (!s.empty() && !cur->Right)
 				{
-					std::cout << s.top()->Key << " ";
+					TakeValue(s.top()->Key);
 					s.pop();
 					if(!s.empty()) cur = s.top();
 				}
 				if (!s.empty() && cur->Right)
 				{
-					std::cout << s.top()->Key << " ";
+					TakeValue(s.top()->Key);
 					s.pop();
 					cur = cur->Right;
 				}
@@ -167,8 +263,12 @@ void BinTree::DFSN(Node* node) const
 			}
 		}
 	}
-	std::cout << '\n';
 	return;
+}
+
+void PrintValue(const int value)
+{
+	std::cout << value << " ";
 }
 
 int main()
@@ -182,8 +282,8 @@ int main()
 		std::cin >> num;
 		MyBinTree.Insert(num);
 	}
+	MyBinTree.DFS(PrintValue);
 
-	MyBinTree.DFS();
 	//system("pause");
 	return 0;
 }
